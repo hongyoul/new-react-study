8698cb89ca91b2cff2a64442a93658b0d9b02ee9
Hong Youl <hongyoul03@gmail.com>
2024-10-30 16:14:52 +0900

    8장 등록 추가

--- react-app8/src/App.js
+++ react-app8/src/App.js
@@ -52,11 +52,28 @@ function Article(props) {
 }
 
 // 등록폼을 반환하는 컴포넌트
-function Create() {
+function Create(props) {
+
+  // 부모쪽에서 전달받은 props 확인
+  // console.log(props);
+
   return(
     <article>
       <h2>Create</h2>
-      <form>
+      {/* Submit 버튼을 클릭하면 전달받은 이벤트 함수를 실행 */}
+      <form onSubmit={
+        (event)=>{
+          event.preventDefault(); // 페이지 이동 방지
+
+          // 폼 데이터 꺼내기
+          // 1. console.log(event.target); => console에서 확인가능
+
+          // 2. 확인 후 값 꺼내기
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }
+      }>
         <p>
           {/* 제목 */}
           <input type="text" name="title" placeholder='title'></input> 
@@ -80,14 +97,17 @@ function App() {
   let [mode, setMode] = useState('WELCOME'); 
 
   let [id, setId] = useState(null);
+  
+  // 게시물을 등록할 때 사용할 ID (ID값이 마지막 ID값에서 1만큼 증가)
+  let [nextid, setNextid] = useState(4);
 
   let content = null;
 
-  const topics = [
+  let [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'},
-  ];
+  ]);
 
   if(mode === "WELCOME") {
     content = <Article title="Welcome" body="Hello, Web"></Article>
@@ -107,8 +127,30 @@ function App() {
     content=<Article title={title} body={body}></Article>
 
   } else if(mode === "CREATE") {
-    // 모드가 CREATE라면, 등록 컴포넌트를 반환
-    content = <Create></Create>
+    //1번
+    // 모드가 CREATE라면, 등록 컴포넌트를 반환(형식만 만들어짐)
+    // content = <Create></Create>
+
+    // 2번
+    // Create 컴포넌트에서 등록 버튼을 클릭하면 게시물이 등럭되는 이벤트 추가
+    content = <Create onCreate={
+      (title, body) => {
+        // 기존 배열으 복사하여 새로운 배열로 생성
+        const newTopics = [...topics];
+        // console.log(title, body);
+
+        let newTopic = {id:nextid, title:title, body:body}
+
+        newTopics.push(newTopic);
+
+        setTopics(newTopics); // 100번지 -> 200번지
+
+        // 다음 아이디를 1만큼 증가 시키기
+        setNextid(nextid +1);
+      }
+    }>
+
+    </Create>
   }
   
   return (
