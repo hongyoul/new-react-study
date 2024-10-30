3778f2adff37b3c590be28509cb8bfd6f6e6bd59
Hong Youl <hongyoul03@gmail.com>
2024-10-30 10:03:05 +0900

    6번 파일 추가

--- react-app6/src/App.js
+++ react-app6/src/App.js
@@ -6,13 +6,15 @@ function Header(props) {
   return (
     <header>
         <h1><a href='/' onClick={
-          () => {}
+          (event) => {
+            event.preventDefault(); // 페이지 이동방지
+            props.onChangeMode(); // props로 전달 받은 이벤트 함수 실행
+          }
         }>{props.title}</a></h1>
     </header>
     
   );
 }
-
 
 function Nav(props) {
 
@@ -20,7 +22,16 @@ function Nav(props) {
 
   for(let t of props.topics) {
 
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+    lis.push(<li key={t.id}>
+      <a href={'/read/'+t.id} id={t.id} onClick={
+        (event) => {
+          event.preventDefault(); // 이벤트 객체로 페이지 이동방지
+          console.log(event.target.id); // 클릭했을 때 열릴 위치를 명시
+          props.onChangeMode(event.target.id); // props로 전달받은 함수 호출
+        }
+      }>{t.title}
+      </a>
+      </li>);
   }
 
   return (
@@ -59,7 +70,12 @@ function App() {
       }
     }></Header>
 
-    <Nav topics={topics}></Nav>
+    {/* Nav 컴포넌트에 이벤트 기능 추가 클릭하면 해당 id가 경고창으로 출력 */}
+    <Nav topics={topics} onChangeMode={
+      (id) => {
+        alert(id);
+      }
+    }></Nav>
 
     <Article title="Welcome" body="Hello, Web"></Article>
     </div>
